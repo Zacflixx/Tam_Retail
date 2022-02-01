0706f3c5f006d300bbb8bac570d86f705b808f1f
Isaac Conteh <zacflixx2917@gmail.com>
2022-02-01 05:43:40 +0000

    New Updates on Tamawal Retail

---
+++ js/cat.js
@@ -0,0 +1,15 @@
+document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
+
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+        invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+        el: '.blog-slider__pagination',
+        clickable: true,
+    }
+});
